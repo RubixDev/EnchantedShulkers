51e74a94872f38628400ebe910329c766603a41d
RubixDev <silas.groh@t-online.de>
2023-12-11 21:39:44 +0800

    fix: change mixin priorities for decent compat with ETF
    fixes #35

--- src/main/java/de/rubixdev/enchantedshulkers/mixin/client/ShulkerBoxBlockEntityRendererMixin.java
+++ src/main/java/de/rubixdev/enchantedshulkers/mixin/client/ShulkerBoxBlockEntityRendererMixin.java
@@ -12,32 +12,15 @@ import net.minecraft.client.render.entity.model.ShulkerEntityModel;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.DyeColor;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static de.rubixdev.enchantedshulkers.ClientMod.*;
 
-@Mixin(ShulkerBoxBlockEntityRenderer.class)
+@Mixin(value = ShulkerBoxBlockEntityRenderer.class, priority = 1001)
 public class ShulkerBoxBlockEntityRendererMixin {
-    @ModifyVariable(
-            method = "render(Lnet/minecraft/block/entity/ShulkerBoxBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V"),
-            ordinal = 0)
-    private SpriteIdentifier modifySpriteIdentifier(SpriteIdentifier value, ShulkerBoxBlockEntity shulkerBoxBlockEntity, float f) {
-        if (!ClientMod.customModels()
-                || !Utils.shouldGlint(shulkerBoxBlockEntity)
-                || shulkerBoxBlockEntity.getAnimationProgress(f) > 0.01f
-                || Utils.hasTwoColors(shulkerBoxBlockEntity)) return value;
-        DyeColor dyeColor;
-        return (dyeColor = shulkerBoxBlockEntity.getColor()) == null
-                ? CLOSED_SHULKER_TEXTURE_ID
-                : CLOSED_COLORED_SHULKER_BOXES_TEXTURE_IDS.get(dyeColor.getId());
-    }
-
     @Redirect(
             method =
                     "render(Lnet/minecraft/block/entity/ShulkerBoxBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
---
+++ src/main/java/de/rubixdev/enchantedshulkers/mixin/client/ShulkerBoxBlockEntityRendererMixin_999.java
@@ -0,0 +1,31 @@
+package de.rubixdev.enchantedshulkers.mixin.client;
+
+import de.rubixdev.enchantedshulkers.ClientMod;
+import de.rubixdev.enchantedshulkers.Utils;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
+import net.minecraft.client.render.block.entity.ShulkerBoxBlockEntityRenderer;
+import net.minecraft.client.util.SpriteIdentifier;
+import net.minecraft.util.DyeColor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import static de.rubixdev.enchantedshulkers.ClientMod.*;
+
+@Mixin(value = ShulkerBoxBlockEntityRenderer.class, priority = 999)
+public class ShulkerBoxBlockEntityRendererMixin_999 {
+    @ModifyVariable(
+            method = "render(Lnet/minecraft/block/entity/ShulkerBoxBlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;push()V", shift = At.Shift.BEFORE),
+            ordinal = 0)
+    private SpriteIdentifier modifySpriteIdentifier(SpriteIdentifier value, ShulkerBoxBlockEntity shulkerBoxBlockEntity, float f) {
+        if (!ClientMod.customModels()
+                || !Utils.shouldGlint(shulkerBoxBlockEntity)
+                || shulkerBoxBlockEntity.getAnimationProgress(f) > 0.01f
+                || Utils.hasTwoColors(shulkerBoxBlockEntity)) return value;
+        DyeColor dyeColor;
+        return (dyeColor = shulkerBoxBlockEntity.getColor()) == null
+                ? CLOSED_SHULKER_TEXTURE_ID
+                : CLOSED_COLORED_SHULKER_BOXES_TEXTURE_IDS.get(dyeColor.getId());
+    }
+}
